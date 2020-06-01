74c2d4a6164729256e3fb7131c4bba553b6ccc94
Gustice <Jakob.Ger@gmail.com>
2020-06-01 02:20:12 +0200

    fix: German translated to English

--- C/For_While/main.c
+++ C/For_While/main.c
@@ -17,7 +17,7 @@
 int num1 = 0;
 int num2 = 1;
 int fibo = 0;
-int i = 0 ;
+int i = 0;
 int z = 2000000000;
 
 void for_fcn(void);
@@ -25,10 +25,10 @@ void while_fcn(void);
 
 void for_fcn(void)
 {
-    for(i=0; i<z; i++)
+    for (i = 0; i < z; i++)
     {
         fibo = num1 + num2;
-        //Sprintf("Fibonacci-Folge: %d \n", fibo);
+        //Sprintf("Fibonacci-sequence: %d \n", fibo);
         num1 = num2;
         num2 = fibo;
     }
@@ -36,16 +36,15 @@ void for_fcn(void)
 
 void while_fcn(void)
 {
-    while(i<z)
+    while (i < z)
     {
         fibo = num1 + num2;
-        //printf("Fibonacci-Folge: %d \n", fibo);
-        num1 = num2;  
+        //printf("Fibonacci-sequence: %d \n", fibo);
+        num1 = num2;
         num2 = fibo;
         i++;
     }
 }
-
 
 /* Main to call all functions */
 int main(int argc, char const *argv[])
@@ -55,38 +54,37 @@ int main(int argc, char const *argv[])
     int TimeFor = 0;
     int TimeWhile = 0;
 
-    printf("FOR-Schleife: \n");
+    printf("FOR-loop: \n");
     timeStart = clock();
     for_fcn();
     timeEnd = clock();
-    printf("Anzahl Berechnungen: %d\n", i);
-    TimeFor = timeEnd-timeStart;
-    printf("Start: %d, Ende: %d, Dauer: %d\n", timeStart, timeEnd, TimeFor);
+    printf("Number of calculations: %d\n", i);
+    TimeFor = timeEnd - timeStart;
+    printf("Start: %d, End: %d, Duration: %d\n", timeStart, timeEnd, TimeFor);
     num1 = 0;
     num2 = 1;
-    i = 0 ;
+    i = 0;
 
-
-    printf("WHILE-Schleife: \n");
+    printf("WHILE-loop: \n");
     timeStart = clock();
     while_fcn();
     timeEnd = clock();
-    printf("Anzahl Berechnungen: %d\n", i);
-    TimeWhile = timeEnd-timeStart;
-    printf("Start: %d, Ende: %d, Dauer: %d\n", timeStart, timeEnd, TimeWhile);
+    printf("Number of calculations: %d\n", i);
+    TimeWhile = timeEnd - timeStart;
+    printf("Start: %d, End: %d, Duration: %d\n", timeStart, timeEnd, TimeWhile);
 
-    if(TimeFor > TimeWhile)
+    if (TimeFor > TimeWhile)
     {
-        printf("\nDie WHILE-Schleife ist schneller !!!\n");
+        printf("\nWHILE-loop is faster!!!\n");
     }
     else if (TimeFor < TimeWhile)
     {
-        printf("\nDie FOR-Schleife ist schneller !!!\n");
+        printf("\nFOR-loop is faster!!!\n");
     }
     else
     {
-        printf("\nFOR- und WHILE-Schleife sind gleich schnell !!!\n");
-    } 
+        printf("\nFOR- and WHILE-loop are equally fast!!!\n");
+    }
 
     return 0;
 }
