bb090ef827e4a13eadaf2fe692ab10890f4d916a
Gustice <Jakob.Ger@gmail.com>
2020-10-30 22:10:38 +0100

    tests: CMake-Tests added to project
    (still don't understand them)

--- CMake/DemoProject/app/main.cpp
+++ CMake/DemoProject/app/main.cpp
@@ -6,11 +6,14 @@
  * @param argc 
  * @param argv 
  * @return int 
+ * 
+ * @todo Next Step 5
  */
 
 
 #include <stdio.h>
 #include <string>
+#include <iostream>
 
 #include "Version.h"
 #include "Config.h"
@@ -45,5 +48,16 @@ int main(int argc, char const *argv[])
     printf("Returned from call %d \n ", Prc_ProcessCall(1));
     PrintUsageOfStaticLib();
     
+
+    if (argc < 2) {
+        std::cout << "Error: Too little arguments"; 
+        return 1;
+    }
+
+    const double inputValue = std::stod(argv[1]);
+    double outputValue = inputValue;
+    std::cout << "The result of " << inputValue << " is " << outputValue
+            << std::endl;
+
     return 0;
 }
